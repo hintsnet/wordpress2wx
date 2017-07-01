8d6fbbfcfac6197e4b5814009d95f482bf747001
pimgeek <wudi.biz@gmail.com>
2017-07-01 09:54:13 +0800

    修改 app.js 注释信息

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 /*
- * 
- * WordPres版微信小程序
- * author: jianbo
- * organization: 守望轩  www.watch-life.net
- * github:    https://github.com/iamxjb/winxin-app-watch-life.net
- * 技术支持微信号：iamxjb
- * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
+ * WordPress 转微信小程序
+ * author: pimgeek
+ * original author: jianbo
+ * original github repo: https://github.com/iamxjb/winxin-app-watch-life.net
  * 
  */
 
 App({
-  onLaunch: function () {
+  onLaunch: function(){
     // 调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-  getUserInfo:function(cb){
+  getUserInfo: function(cb){
     var that = this
-    if (this.globalData.userInfo) {
+    if (this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
     } else {
       // 调用登录接口
       wx.login({
-        success: function () {
+        success: function (){
           wx.getUserInfo({
-            success: function (res) {
+            success: function(res){
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
@@ -34,7 +31,7 @@ App({
       })
     }
   },
-  globalData:{
+  globalData: {
     userInfo:null
   }
 })
