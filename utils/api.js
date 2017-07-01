6a258dbe7ddcfd45efd234b4ee6cfc4764d40615
pimgeek <wudi.biz@gmail.com>
2017-07-02 03:43:57 +0800

    utils/api.js 代码格式微调，修改版权说明信息

--- utils/api.js
+++ utils/api.js
@@ -1,20 +1,16 @@
 /*
- * 
- * WordPres版微信小程序
- * author: jianbo
- * organization: 守望轩  www.watch-life.net
- * github:    https://github.com/iamxjb/winxin-app-watch-life.net
- * 技术支持微信号：iamxjb
- * 开源协议：MIT
- * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
+ * WordPress 转微信小程序
+ * author               : pimgeek
+ * original author      : jianbo
+ * original github repo : https://github.com/iamxjb/winxin-app-watch-life.net
+ * open source license  : MIT - https://choosealicense.com/licenses/mit/
  */
 
 var HOST_URI = 'https://www.watch-life.net/wp-json/wp/v2/';
 
-
 module.exports = {
   // 获取文章列表数据
-  getPosts: function (obj) {
+  getPosts: function(obj) {
     var url = HOST_URI + 'posts?per_page=6&page=' + obj.page;
     
     if (obj.categories != 0) {
@@ -27,58 +23,59 @@ module.exports = {
 
   },
 
-  getStickyPosts: function () {
+  getStickyPosts: function() {
     var url = HOST_URI + 'posts?sticky=true&per_page=5&page=1';
     return url;
 
   },
 
-
   // 获取特定id的文章列表
-  getPostsByIDs: function (obj) {
+  getPostsByIDs: function(obj) {
     var url = HOST_URI + 'posts?include=' + obj;
 
     return url;
 
   },
+
   // 获取内容页数据
-  getPostByID: function (id, obj) {
+  getPostByID: function(id, obj) {
     
     return HOST_URI + 'posts/' + id;
   },
+
   // 获取页面列表数据
-  getPages: function () {
+  getPages: function() {
     
     return HOST_URI + 'pages';
   },
 
   // 获取页面列表数据
-  getPageByID: function (id, obj) {
+  getPageByID: function(id, obj) {
     return HOST_URI + 'pages/' + id;
   },
+
   //获取分类列表
-  getCategories: function () {
+  getCategories: function() {
     return HOST_URI + 'categories?per_page=50&orderby=count&order=desc'
   },
+
   //获取评论
-  getComments: function (obj) {
+  getComments: function(obj) {
     return HOST_URI + 'comments?per_page=6&orderby=date&order=asc&post=' + obj.postID + '&page=' + obj.page
   },
 
   //获取最近的50个评论
-  getRecentfiftyComments:function(){
+  getRecentfiftyComments: function() {
     return HOST_URI + 'comments?per_page=30&orderby=date&order=desc'
   },
 
   //获取最近的50个评论
-  postComment: function () {
+  postComment: function() {
     return HOST_URI + 'comments'
   },
 
-   
-
   //获取文章的第一个图片地址,如果没有给出默认图片
-  getContentFirstImage: function (content){
+  getContentFirstImage: function(content) {
     var regex = /<img.*?src=[\'"](.*?)[\'"].*?>/i;
     var arrReg = regex.exec(content);
     var src ="../../images/watch-life-logo-128.jpg";
@@ -87,5 +84,4 @@ module.exports = {
     }
     return src;  
   }
-
 };
